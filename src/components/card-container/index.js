a12ac4d66576f361371fba654750089603e971a9
Guest <abhishek.surbhat1@wipro.com>
2019-05-17 19:09:23 +0530

    added final view cards.

--- src/components/card-container/index.js
+++ src/components/card-container/index.js
@@ -30,13 +30,22 @@ class CardContainer extends React.Component {
     }
   }
 
+  getDate = (date) => {
+    let reverseDate = date.match("[0-9]{4}[-][0-9]{2}[-][0-9]{2}");
+    let temp = reverseDate[0].split("-").reverse();
+    let finalDate = temp.join("-");
+
+    return finalDate;
+  }
+
   render () {
     let { cityData } = this.props;
+    this.getDate(cityData.list[0].dt_txt)
     return (
       <div className="row justify-content-md-center">
       <div className="col-md-auto">
         <div className="card mt-5">
-          <div className="card-header d-flex flex-row flex-wrap" 
+          <div className="card-header d-flex flex-row justify-content-between" 
             style={{background:
               this.getBackground(cityData.list[0].weather[0].main)}}>
             <div>
@@ -46,10 +55,10 @@ class CardContainer extends React.Component {
                  height="75" />
               <h3 className="card-title">{cityData.city.name}</h3>
               <p className="card-text ">
-                {cityData.list[0].weather[0].description}
+                {cityData.list[0].weather[0].description.toUpperCase()}
               </p>
             </div>
-            <div className="align-self-center ml-2">
+            <div className="align-self-center ml-5">
               <div className="d-flex flex-row">
                 <h5 className="card-title">Temperature: </h5>
                 <p className="ml-2">
@@ -62,14 +71,25 @@ class CardContainer extends React.Component {
                   {(cityData.list[0].wind.speed*3600)/1000} kph
                 </p>
               </div>
+              <div className="d-flex flex-row">
+                <h5 className="card-text">Date: </h5>
+                <p className="ml-2">
+                  {this.getDate(cityData.list[0].dt_txt)}
+                </p>
+              </div>
             </div>
           </div>
-          <div className="card-body d-flex flex-wrap">
+          <div
+            className="card-body d-flex flex-wrap p-0"
+            style={{background: '#9ab1cc'}}>
           {cityData.list.map((city, index) => {
             if(index > 4) return null;
             else {
               return (
-                  <SubCards cityData={city} />
+                  <SubCards key={index} cityData={city}
+                    getBackground={this.getBackground}
+                    getIcon={this.getIcon}
+                    getDate={this.getDate}/>
               )
             }
           })}
--- src/components/sub-cards/index.js
+++ src/components/sub-cards/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const SubCards = () => {
+const SubCards = (props) => {
   return (
-      <div className="">
-        <p>Hello body</p>
+      <div className="d-flex flex-column m-1 p-3 justify-content-center" 
+        style={{background: 
+          props.getBackground(props.cityData.weather[0].main),
+          border: 'solid 1px gray'}}>
+        <img src={props.getIcon(props.cityData.weather[0].main)}
+          width="50px"
+          height="50px"
+          alt="Weather Icon"/>
+        <p>{props.cityData.main.temp} °C</p>
+        <p>{props.getDate(props.cityData.dt_txt)}</p>
       </div>
     
   )
