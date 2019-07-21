d52ce13b00c0c44247f4afbfb1af6a14192aed52
abhisheksurbhat <abhishek.surbhat@gmail.com>
2019-07-21 14:26:03 +0530

    first commit

--- src/components/card-container/index.js
+++ src/components/card-container/index.js
@@ -30,42 +30,51 @@ class CardContainer extends React.Component {
     }
   }
 
+  getDate = (date) => {
+    let reverseDate = date.match("[0-9]{4}[-][0-9]{2}[-][0-9]{2}");
+    let temp = reverseDate[0].split("-").reverse();
+    let finalDate = temp.join("-");
+
+    return finalDate;
+  }
+
   render () {
     let { cityData } = this.props;
+    this.getDate(cityData.list[0].dt_txt)
     return (
       <div className="row justify-content-md-center">
       <div className="col-md-auto">
         <div className="card mt-5">
           <div className="card-header d-flex flex-row justify-content-between" 
             style={{background:
-              this.getBackground(cityData[0].main)}}>
+              this.getBackground(cityData.list[0].weather[0].main)}}>
             <div>
-              <img src={this.getIcon(cityData[0].main)}
+              <img src={this.getIcon(cityData.list[0].weather[0].main)}
                  alt="Weather Icon Here"
                  width="75"
                  height="75" />
-              <h3 className="card-title">{cityData[0].city}</h3>
+              <h3 className="card-title">{cityData.city.name}</h3>
               <p className="card-text ">
-                {cityData[0].description.toUpperCase()}
+                {cityData.list[0].weather[0].description.toUpperCase()}
               </p>
             </div>
             <div className="align-self-center ml-5">
               <div className="d-flex flex-row">
                 <h5 className="card-title">Temperature: </h5>
                 <p className="ml-2">
-                  {cityData[0].temp.toFixed(2)} °C
+                  {cityData.list[0].main.temp} °C
                 </p>
               </div>
               <div className="d-flex flex-row">
                 <h5 className="card-text">Wind Speed: </h5>
                 <p className="ml-2">
-                  {((cityData[0].wind*3600)/1000).toFixed(2)} kph
+                  {(cityData.list[0].wind.speed*3600)/1000} kph
                 </p>
               </div>
               <div className="d-flex flex-row">
                 <h5 className="card-text">Date: </h5>
                 <p className="ml-2">
-                  {cityData[0].date}
+                  {this.getDate(cityData.list[0].dt_txt)}
                 </p>
               </div>
             </div>
@@ -73,14 +82,14 @@ class CardContainer extends React.Component {
           <div
             className="card-body d-flex flex-wrap p-0"
             style={{background: '#9ab1cc'}}>
-          {cityData.map((city, index) => {
-            if(index === 0) return null;
+          {cityData.list.map((city, index) => {
+            if(index > 4) return null;
             else {
               return (
                   <SubCards key={index} cityData={city}
                     getBackground={this.getBackground}
                     getIcon={this.getIcon}
-                  />
+                    getDate={this.getDate}/>
               )
             }
           })}
